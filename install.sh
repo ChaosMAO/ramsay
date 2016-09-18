a63028f6a2b14ca3ff92029a563838faf2ef99d9
Andrea Cardaropoli <chaosmao@localhost.localdomain>
2016-09-18 13:53:32 +0100

    Fixed installer script

--- install.sh
+++ install.sh
@@ -3,20 +3,15 @@ dir="/opt/ramsay"
 
 if [[ ! -e $dir ]]; then
     mkdir $dir
+    mv * /opt/ramsay/
 elif [[ ! -d $dir ]]; then
     echo "$dir already exists but is not a directory" 1>&2
 fi
 
-if [ "$(ls -A $DIR)" ]; then
-     echo "Looks like you already have installed Ramsay"
-else
-	mv * /opt/ramsay/
-fi
-
 if [ -f /usr/bin/ramsay ]; then
-	ln -s /opt/ramsay/ramsay.rb /usr/bin/ramsay
-else
 	echo "File /usr/bin/ramsay exists already."
+else
+	ln -s /opt/ramsay/ramsay.rb /usr/bin/ramsay
 fi
 
 if [ ! -f /var/log/ramsay.log ]; then
